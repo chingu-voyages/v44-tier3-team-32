0cadbf02616be5f82a3c821c32669e47b69666fe
DannyVogel <danny.hj.vogel@gmail.com>
2023-05-22 16:11:22 +0200

    add menu items, logo and account icons to header; add styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,62 @@
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import "./Header.css";
 
 export default function Header() {
+  const [womenDropdownOpen, setWomenDropdownOpen] = useState(false);
+  const [menDropdownOpen, setMenDropdownOpen] = useState(false);
+
+  const toggleWomenDropdown = () => {
+    setWomenDropdownOpen(!womenDropdownOpen);
+  };
+
+  const toggleMenDropdown = () => {
+    setMenDropdownOpen(!menDropdownOpen);
+  };
+
+
 
   return (
     <header>
-      <h1>Site Logo</h1>
-      <nav></nav>
+      <nav className="menu">
+        <ul className="menu-list">
+          <li className="menu-item">
+            <a href="#" className="menu-link" onClick={toggleWomenDropdown}>
+              Women
+            </a>
+            {womenDropdownOpen && (
+              <ul className="dropdown">
+                <li className="dropdown-item">Shirts</li>
+                <li className="dropdown-item">Pants</li>
+                <li className="dropdown-item">Jackets</li>
+                <li className="dropdown-item">Hats</li>
+                <li className="dropdown-item">Shoes</li>
+              </ul>
+            )}
+          </li>
+          <li className="menu-item">
+            <a href="#" className="menu-link" onClick={toggleMenDropdown}>
+              Men
+            </a>
+            {menDropdownOpen && (
+              <ul className="dropdown">
+                <li className="dropdown-item">Shirts</li>
+                <li className="dropdown-item">Pants</li>
+                <li className="dropdown-item">Jackets</li>
+                <li className="dropdown-item">Hats</li>
+                <li className="dropdown-item">Shoes</li>
+              </ul>
+            )}
+          </li>
+        </ul>
+      </nav>
+      <h1 className="logo">The Store</h1>
+      <div className="iconContainer">
+        <i className="fa-regular fa-heart"></i>
+        <i className="fa-solid fa-magnifying-glass"></i>
+        <i className="fa-regular fa-user"></i>
+      </div>
+
     </header>
   );
 }
