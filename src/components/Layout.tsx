eb65ba1f0726f38617743ee98d2528e47a08851a
LeRoi Vladimir <banyamkimbi@vrfound.org>
2023-05-22 16:17:01 +0100

    hero section

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,13 +2,14 @@ import { Outlet } from "react-router-dom"
 import Header from "./Header"
 import Footer from "./Footer"
 import "../styles.css"
+import Hero from "./Hero"
 
 export default function Layout() {
     return (
         <div className="site-wrapper">
             <Header />
             <main>
-                <Outlet />
+                <Hero />
             </main>
             <Footer />
         </div>
